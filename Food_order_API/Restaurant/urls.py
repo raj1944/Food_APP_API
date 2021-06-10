291e9f80f0e73b6105bb40c0d3a4dbc1da2f7d6e
raj1944 <rajakbari441999@gmail.com>
2021-06-10 17:45:42 +0530

    commit

--- Food_order_API/Restaurant/urls.py
+++ Food_order_API/Restaurant/urls.py
@@ -4,6 +4,11 @@ from .views import RestaurantViewSet, BranchViewSet, FoodViewSet, MenuViewSet, O
 
 
 urlpatterns = [
+    path('menu/add/', MenuViewSet.as_view()),
+    path('menu/update/<id>', MenuViewSet.as_view()),
+    path('order/', OrderViewSet.as_view()),
+    path('order/<id>', OrderViewSet.as_view()),
+
     path('user/', UserViewSet.as_view()),
     path('user/<id>', UserViewSet.as_view()),
     path('restaurant/', RestaurantViewSet.as_view()),
@@ -12,9 +17,5 @@ urlpatterns = [
     path('branch/<id>', BranchViewSet.as_view()),
     path('food/', FoodViewSet.as_view()),
     path('food/<id>/', FoodViewSet.as_view()),
-    path('menu/', MenuViewSet.as_view()),
-    path('menu/<id>', MenuViewSet.as_view()),
-    path('order/', OrderViewSet.as_view()),
-    path('order/<id>', OrderViewSet.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
